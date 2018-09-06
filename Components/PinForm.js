ffec664102669b9c60146391386ec1741eb43072
Kimiko Motoyama <motoyamakimiko2000@gmail.com>
2018-09-06 12:13:15 +0900

    form shows up, no submit button yet

--- Components/PinForm.js
+++ Components/PinForm.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements';
 
 export default class PinForm extends React.Component {
+  _handleChange(event) {
+    console.log("changed");
+  }
+
   render() {
     return (
-      <View>
-        <Text>I'm PinForm</Text>
+      <View style={styles.container}>
+        <View style={styles.item}>
+          <FormLabel>Create Pin</FormLabel>
+          <FormInput onChangeText={this._handleChange}/>
+          <FormValidationMessage>{'This field is required'}</FormValidationMessage>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'flex-start'
+  },
+  item: {
+    marginTop: 50,
+    width: 350,
+  }
+});
