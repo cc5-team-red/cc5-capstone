27115952b813dc317da8c587bc99252f6c5e84e2
Code Chrysalis <dustin.andy.tran@gmail.com>
2018-09-08 19:06:37 +0900

    refactor: removed unneeded state and dependencies.
    Co-authored-by: Dustin Tran <dustin.andy.tran@gmail.com>
    Co-authored-by: Tsubasa Kondo <tsubasa.k111@gmail.com>

--- Components/MapScreen.js
+++ Components/MapScreen.js
@@ -1,26 +1,17 @@
 import React from "react";
-import { Platform, Text, View, StyleSheet, Button } from "react-native";
-import { MapView, Constants, Location, Permissions } from "expo";
+import { Text, View, StyleSheet, Button } from "react-native";
 import Map from "./Map.js";
-
-import { createPin, pinListener } from "../firebase/helper";
 
 export default class MapScreen extends React.Component {
   // for react-navigator
   static navigationOptions = {
     title: "Map"
   };
-
-  state = {
-    location: null,
-    errorMessage: null,
-  };
  
   render() {
-    console.log('MapScreen props:');
-    console.log(this.props);
-    
-    // createPin(this.state.pins[0]);
+    // console.log('MapScreen props:');
+    // console.log(this.props);
+    // createPin(this.props.screenProps.pins[0]);
     // pinListener();
 
     const { navigate } = this.props.navigation;
