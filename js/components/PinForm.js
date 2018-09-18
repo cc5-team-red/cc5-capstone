9b9d86fb97d31cbeb2ee21856c1f9713a7db6da7
Dustin <dustin.tran@live.com>
2018-09-18 20:32:33 +0900

    feat: render IOSActionSheet for iOS devices. Initialize pin type to fix bug

--- js/components/PinForm.js
+++ js/components/PinForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View, Picker } from "react-native";
+import { Platform, ActionSheetIOS, StyleSheet, Text, View, Picker } from "react-native";
 import {
   FormLabel,
   FormInput,
@@ -18,7 +18,35 @@ export default class PinForm extends React.Component {
     this.props.screenProps._submitPinForm(event);
   }
 
+  _showActionSheet = () => {
+    ActionSheetIOS.showActionSheetWithOptions({
+      options: ['no passage', 'danger', 'help', 'medical facility', 'fire', 'cancel'], 
+      cancelButtonIndex: 5,
+      title: "Pin Type",
+      message: "Please Select A Type"},
+    (buttonIndex) => {
+      if (buttonIndex === 0) {
+        this.props.screenProps._onChangeType("no_passage");
+      }
+      if (buttonIndex === 1) {
+        this.props.screenProps._onChangeType("danger");
+      }
+      if (buttonIndex === 2) {
+        this.props.screenProps._onChangeType("help");
+      }
+      if (buttonIndex === 3) {
+        this.props.screenProps._onChangeType("medical");
+      }
+      if (buttonIndex === 4) {
+        this.props.screenProps._onChangeType("fire");
+      }
+    } )
+  }
+
   render() {
+    if(!this.props.screenProps.newPin.type){
+      this.props.screenProps._onChangeType("no_passage");
+    }
     return (
       <View style={styles.container}>
         <View style={styles.item}>
@@ -29,17 +57,25 @@ export default class PinForm extends React.Component {
           </FormValidationMessage>
 
           <FormLabel>Type</FormLabel>
-          <Picker
+          {Platform.OS === "android" ?
+          ( <Picker
             selectedValue={this.props.screenProps.newPin.type}
             style={{ height: 50, width: 100 }}
             onValueChange={this.props.screenProps._onChangeType}
           >
-            <Picker.Item label="no_passage" value="no_passage" />
+            <Picker.Item label="no passage" value="no_passage" />
             <Picker.Item label="danger" value="danger" />
             <Picker.Item label="help" value="help" />
             <Picker.Item label="medical facility" value="medical" />
             <Picker.Item label="fire" value="fire" />
-          </Picker>
+          </Picker> ) 
+          : (
+            <View>
+              <FormLabel>{`Selected: ${this.props.screenProps.newPin.type}`}</FormLabel>
+              <Button style={styles.button} title="Select Type" onPress={this._showActionSheet} />
+            </View>
+          )
+          }
 
           <FormLabel>Details</FormLabel>
           <FormInput value={this.props.screenProps.newPin.details} onChangeText={this.props.screenProps._onChangeDetails} />
