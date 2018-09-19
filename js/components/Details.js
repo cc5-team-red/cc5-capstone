f681edb59a37af2f87b744382b15e7bb67010478
Dustin <dustin.tran@live.com>
2018-09-19 15:40:48 +0900

    feat: moved title to upper banner. Spaced out components in View

--- js/components/Details.js
+++ js/components/Details.js
@@ -4,14 +4,19 @@ import { pinListener, upvotePin, downvotePin, commentPin } from "../firebase/hel
 import PinComment from "./PinComment";
 
 export default class Details extends React.Component {
-  static navigationOptions = {
-    title: "Details"
-  };
   state = {
     comment: "",
     voted: false,
+    id: this.props.navigation.getParam("id"),
+    title: this.props.navigation.getParam("title"),
+    votes: this.props.navigation.getParam("votes"),
+    hoursAgo: this.props.navigation.getParam("hoursAgo"),
+    details: this.props.navigation.getParam("details"),
   }
 
+  static navigationOptions = ({navigation}) => ({
+    title: `${navigation.state.params.title}`,
+  });
 
   _handleChange = (input) => {
     this.setState({ comment: input });
@@ -63,31 +68,23 @@ export default class Details extends React.Component {
   }
 
   render() {
-    const navigation = this.props.navigation;
-    const id = navigation.getParam("id");
-    const title = navigation.getParam("title");
-    const votes = navigation.getParam("votes");
-    const hoursAgo = navigation.getParam("hoursAgo");
-    let details = navigation.getParam("details");
-    if (!details){
-      details = "None"
-    }
-
     return (
-      <View>
-        <Text style={{alignSelf:"center", fontWeight:"bold", fontSize:20}}>{title}</Text>
-        {this._showVotes(id)}
-        <Text>{`Last Updated: ${hoursAgo >= 1 ? (`Updated ${hoursAgo} hours ago`)
-        : (`Updated ${(hoursAgo*60).toFixed(0)} minutes ago`)} \nDetails: ${details}\n`}</Text>
-        <Text>{`Comments:`}</Text>
-        <View style={{maxHeight:"70%"}}>
-          <ScrollView style={{flexGrow:0}}>
-            {this._showComments(id)}
+      <View style={{flex:1}}>
+        <View style={{flex:2}}>
+          {this._showVotes(this.state.id)}
+          <Text>{`Last Updated: ${this.state.hoursAgo >= 1 ? (`Updated ${this.state.hoursAgo} hours ago`)
+          : (`Updated ${(this.state.hoursAgo*60).toFixed(0)} minutes ago`)}`}</Text>
+          <Text>{`Details: ${this.state.details ? (this.state.details) : (`None`)}\n`}</Text>
+          <Text>{`Comments:`}</Text>
+        </View>
+        <View style={{flex:5}}>
+          <ScrollView>
+            {this._showComments(this.state.id)}
           </ScrollView>
         </View>
-        <View style={{flex:1}}>
-          <Button title="Upvote" disabled={this.state.voted} onPress={() => this._sendUpvote(id, votes)} />
-          <Button title="Downvote" disabled={this.state.voted} onPress={() => this._sendDownvote(id, votes)} />
+        <View style={{flex:3}}>
+          <Button title="Upvote" disabled={this.state.voted} onPress={() => this._sendUpvote(this.state.id, this.state.votes)} />
+          <Button title="Downvote" disabled={this.state.voted} onPress={() => this._sendDownvote(this.state.id, this.state.votes)} />
           <PinComment
             comment={this.state.comment}
             _handleChange={this._handleChange}
