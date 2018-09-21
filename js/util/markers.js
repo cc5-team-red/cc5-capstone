1be1553f7715422c98e38478e45095ade2c0e3f1
TsubasaK111 <Tsubasa.K111@gmail.com>
2018-09-21 15:19:33 +0900

    refactor: reuse marker images code in Map to reduce code

--- js/util/markers.js
+++ js/util/markers.js
@@ -7,20 +7,26 @@ import no_passage from "../assets/markers/no_passage.png";
 import crosshairs from "../assets/markers/crosshairs_blue.png";
 import fire from "../assets/markers/fire.png";
 import medical from "../assets/markers/medical.png";
+import blue_user from "../assets/markers/blue_user.png";
 
 export const markers = {
-  sos: sos, 
-  danger: danger, 
-  no_passage: no_passage, 
-  fire: fire, 
-  medical: medical, 
-  crosshairs: crosshairs,
+  help: sos,
+  danger: danger,
+  no_passage: no_passage,
+  fire: fire,
+  medical: medical,
+  user: blue_user,
 }
 
-export const markerImages = Object.values(markers).map(markerSrc => () => (
-  <Image
-    source={markerSrc}
-    resizeMode="contain"
-    height="50"
-  />
-));
+export const markerImages = Object.entries(markers)
+  .filter(([key, value]) => {
+    return (&& key !== "blue_user");
+  })
+  .map(([key, markerSrc]) => () => (
+    <Image
+      source={markerSrc}
+      resizeMode="contain"
+      height="50"
+    />
+  )
+);
