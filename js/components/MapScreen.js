43e7709610361d246cbc026bde394a8c0ce89405
Dustin <dustin.tran@live.com>
2018-09-23 17:05:56 +0900

    also passes pin.user_id to Details

--- js/components/MapScreen.js
+++ js/components/MapScreen.js
@@ -16,8 +16,8 @@ export default class MapScreen extends React.Component {
     this.props.screenProps._setNewCoordinate(e);
   }
 
-  _calloutPressed = (title, id, votes, details, hoursAgo) => {
-    this.props.navigation.navigate("Details", { title, id, votes, details, hoursAgo });
+  _calloutPressed = (title, id, votes, details, hoursAgo, creator) => {
+    this.props.navigation.navigate("Details", { title, id, votes, details, hoursAgo, creator });
   }
 
   _drawPress = async () => {
