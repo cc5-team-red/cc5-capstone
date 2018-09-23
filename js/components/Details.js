44b5da858a236e33ee8c5c5acf914608d1f201c0
Dustin <dustin.tran@live.com>
2018-09-23 17:04:25 +0900

    Add button to delete for pin owner

--- js/components/Details.js
+++ js/components/Details.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, ScrollView, View, Button } from "react-native";
+import { StyleSheet, Text, ScrollView, View, Button, TouchableOpacity } from "react-native";
 import { pinListener, upvotePin, downvotePin, commentPin } from "../firebase/helper"
 import PinComment from "./PinComment";
 
@@ -7,11 +7,13 @@ export default class Details extends React.Component {
   state = {
     comment: "",
     voted: false,
+    confirmDelete: false,
     id: this.props.navigation.getParam("id"),
     title: this.props.navigation.getParam("title"),
     votes: this.props.navigation.getParam("votes"),
     hoursAgo: this.props.navigation.getParam("hoursAgo"),
     details: this.props.navigation.getParam("details"),
+    creator: this.props.navigation.getParam("creator"),
   }
 
   static navigationOptions = ({navigation}) => ({
@@ -51,26 +53,39 @@ export default class Details extends React.Component {
     }
   }
 
+  _deletePin = () => {
+    if(this.props.screenProps.user_id === this.state.creator){
+      this.props.navigation.navigate("Home");
+      this.props.screenProps._deletePin(this.state.id);
+    } else {
+      console.log("id mismatch")
+    }
+  }
+
   _showComments = (id) => {
     const pins = this.props.screenProps.pins.filter(pin => pin.id === id);
-    const comments = pins[0].comments;
-    if (typeof comments === 'object') {
-      return Object.entries(comments).map(([key, value]) => {
-        const timestamp = new Date(value.timestamp);
-        const oneHour = (1000 * 60 * 60)
-        const now = new Date(Date.now())
-        const hoursAgo = ((now - timestamp) / oneHour);
-        return <Text key={key}>{`\n${value.comment.comment}\n ~${hoursAgo >= 1 ? (`Updated ${hoursAgo} hours ago`)
-        : (`Updated ${(hoursAgo*60).toFixed(0)} minutes ago`)}`}</Text>
+    if(pins.length > 0) {
+      const comments = pins[0].comments;
+      if (typeof comments === 'object') {
+        return Object.entries(comments).map(([key, value]) => {
+          const timestamp = new Date(value.timestamp);
+          const oneHour = (1000 * 60 * 60)
+          const now = new Date(Date.now())
+          const hoursAgo = ((now - timestamp) / oneHour);
+          return <Text key={key}>{`\n${value.comment.comment}\n ~${hoursAgo >= 1 ? (`Updated ${hoursAgo} hours ago`)
+          : (`Updated ${(hoursAgo*60).toFixed(0)} minutes ago`)}`}</Text>
+        }
+        )
       }
-      )
     }
   }
 
   _showVotes = (id) => {
     const pins = this.props.screenProps.pins.filter(pin => pin.id === id);
-    const votes = pins[0].votes;
-    return <Text>{`Votes: ${votes}`}</Text>
+    if(pins.length > 0){
+      const votes = pins[0].votes;
+      return <Text>{`Votes: ${votes}`}</Text>
+    }
   }
 
   render() {
@@ -97,6 +112,14 @@ export default class Details extends React.Component {
             _submitPinForm={this._submitPinComment}
             disabled={(this.state.comment === "")}
           />
+          {this.props.screenProps.user_id === this.state.creator && !this.state.confirmDelete ? 
+            (<TouchableOpacity style={{alignItems:"center", backgroundColor: "#F9A800", padding: 15}} onPress={() => (this.setState({confirmDelete: true}))}>
+              <Text style={{color:"#ECECE7", fontSize: 22}}>Delete</Text>
+            </TouchableOpacity>) : null}
+          {this.state.confirmDelete ? 
+            (<TouchableOpacity style={{alignItems:"center", backgroundColor: "#9B2423", padding: 15}} onPress={this._deletePin}>
+              <Text style={{color:"#ECECE7", fontSize: 22}}>Confirm Delete</Text>
+            </TouchableOpacity>) : null}
         </View>
       </View>
     )
